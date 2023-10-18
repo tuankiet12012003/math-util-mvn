fc3d833020258fe9b44f93c0ad3c37716b37a22c
tuankiet12012003 <tuankiet12012003@gmail.com>
2023-10-18 15:07:00 +0700

    update

--- src/test/java/com/tuankiet/mathutil/test/core/MathUtilityDDTTest.java
+++ src/test/java/com/tuankiet/mathutil/test/core/MathUtilityDDTTest.java
@@ -4,23 +4,36 @@
  */
 package com.tuankiet.mathutil.test.core;
 
+import com.tuankiet.mathutil.core.MathUtility;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 /**
  *
  * @author TuanKiet
  */
+
 public class MathUtilityDDTTest {
     
+    //chuẩn bị bộ data, sẽ nhồi vào hàm assert()
+    //hàm trả về mảng 2 chiều, sẽ dùng để so sánh n! có = ? hay ko
     public static Object[][] initData(){
-        return new Integer[][] {
-                                    {0, 1},
-                                    {1, 1},
-                                    {2, 2},
-                                    {3, 6},
-                                    {4, 24},
-                                    {5, 120},
-        };
+        //int [] a ={5, 10, 15, 20, 25, 30};
+        Object[][] testData = {{0, 1}, 
+                               {1, 1}, 
+                               {2, 2}, 
+                               {3, 6}, 
+                               {4, 24}, 
+                               {5, 120}
+                              };
+        return testData;
+    }
+    
+    //nhồi data vào hàm kiểm thử
+    @ParameterizedTest
+    @MethodSource("initData")
+    public void FactorialGivenRightArgumentReturnsOk(int n, long expected) {    
+        assertEquals(expected, MathUtility.getFactorial(n));
     }
 }
